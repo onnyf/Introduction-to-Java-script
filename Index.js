94f459d5efb06673019c77415bf4300014d08d71
onnyf <onnyf2003@gmail.com>
2024-12-10 12:41:23 +0100

    second coomit

---
+++ Index.js
@@ -0,0 +1,92 @@
+// // // variable declearation
+// // var fullName
+// // var married
+// // var numberOfChlidren
+// // var disability
+// // var accountNumber
+
+
+
+// // // variable assingment
+// // fullName ='Olufemi Oni'
+// // married = true
+// // numberOfChlidren = 1
+// // disability = null
+// // accountNumber = undefined
+
+// // variables initialization
+// var fullName = 'Olufemi Oni'
+// var married = true
+// numberOfChlidren = 1
+// disability = null
+// accountNumber = undefined
+
+
+// console.log(fullName)
+// console.log(numberOfChlidren)
+// console.log(20+20)
+// console.log('hello word')
+
+
+// string operators
+var sentence = 'i am going to be a very good software developer'
+console.log(sentence.length)
+console.log(sentence.toUpperCase())
+console.log(sentence.toLowerCase())
+
+
+// concatenation)
+var favouriteTeam = 'manchester united'
+var favouritePlayer  = 'amad'
+var ability = 'very good'
+var sentence2 = 'my favourite player is ' + favouritePlayer + 'he plays for ' + favouriteTeam + 'and is ability is'
+
+
+
+
+// aritimetic operations
+var x = 5
+var y = 3
+var z = 7
+console.log(z-x)
+console.log(x+y)
+console.log(y*x)
+console.log(z/x)
+console.log(z%y)
+console.log(x**y)
+
+// math operators
+console.log(Math.sqrt(144))
+console.log(Math.pow(16,8))
+console.log(Math.round(4.6))
+console.log(Math.round(4.4))
+console.log(Math.floor(4.6))
+console.log(Math.ceil(4.4))
+
+
+// assignment operators
+var num1 = 25
+var num2 = 35
+var num3 = 15
+
+num1 += num3
+console.log(num1)
+num2 -= num3
+num3 -= num2
+console.log(num3)
+
+// comparism operators
+console.log(5==5)
+console.log(5==6)
+console.log(5=='5')
+
+console.log(7==='7')
+console.log(5==='5')
+console.log((6**1)==(2*3))
+console.log(5!=6)
+console.log(5!=='5')
+console.log(100>17)
+console.log(100<17)
+console.log(18<=18)
+
+
